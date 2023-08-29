7b7a868dd8d412f602b1d5ea1af97226c30e71cb
yosshi-0110 <143543155+yosshi-0110@users.noreply.github.com>
2023-08-30 03:18:07 +0900

    Delete dev directory

--- dev/main.tf
+++
@@ -1,5 +0,0 @@
-/** 開発環境の定義 **/
-
-module "instance_compute_engine" {
-  source = "../modules/compute_engine/"
-}
--- dev/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "google" {
-  project = "yoshida-labo"
-  credentials = "yoshida-labo-d9024a688d28.json"
-}
